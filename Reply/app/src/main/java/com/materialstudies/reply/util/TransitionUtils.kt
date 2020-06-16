fb35ae745a562ffc2fb1218421a50c8324ba6e88
Dan Nizri <dniz@google.com>
2020-06-16 12:55:08 -0400

    Todo comment

--- Reply/app/src/main/java/com/materialstudies/reply/util/TransitionUtils.kt
+++ Reply/app/src/main/java/com/materialstudies/reply/util/TransitionUtils.kt
@@ -26,6 +26,8 @@ private const val ELEVATION_SCALE = 0.85F
 /**
  * Create an elevation scale transition that, e.g., can be used in conjunction with a container
  * transform to give the effect that the outgoing screen is receding or advancing along the z-axis.
+ *
+ * TODO: remove once a MaterialElevationScale is available in the library.
  */
 fun createMaterialElevationScale(forward: Boolean): Transition {
     return MaterialSharedAxis(MaterialSharedAxis.Z, forward).apply {
