ec869dee2a1e524dea4fa36255872fc9b10beb4b
hunterstich <hunterstich@google.com>
2020-07-06 13:27:22 -0400

    Removed unused deps

--- Reply/app/src/main/java/com/materialstudies/reply/ui/compose/ComposeFragment.kt
+++ Reply/app/src/main/java/com/materialstudies/reply/ui/compose/ComposeFragment.kt
@@ -17,21 +17,17 @@
 package com.materialstudies.reply.ui.compose
 
 import android.graphics.Color
-import android.graphics.drawable.InsetDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.activity.OnBackPressedCallback
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.transition.Slide
 import androidx.transition.TransitionManager
-import com.google.android.material.chip.Chip
-import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.transition.MaterialContainerTransform
 import com.materialstudies.reply.R
 import com.materialstudies.reply.data.Account
